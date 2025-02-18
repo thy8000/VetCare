4e7457bd2844206f4537995712dae4e4805fbdfb
thy8000 <thunaymoreiradesoares@gmail.com>
2025-02-17 23:29:25 -0300

    🚧 Views: Client & Footer

---
+++ vet-care/resources/views/client/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.main')
+@section('body')
+
+<div class="alert alert-success" role="alert">
+   <h2>Clients</h2>
+</div>
+
+<section class="content">
+   <div class="row">
+      <div class="col-md-12">
+         <div class="box box-primary">
+            <div class="box-header">
+               <a href="/client/new" class="btn btn-success">New Client</a>
+            </div>
+
+            <br>
+
+            <div class="box-body no-padding">
+
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+@endsection
