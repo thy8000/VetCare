927d37455ceff31f0a266e01eff4c51797cbad31
thy8000 <thunaymoreiradesoares@gmail.com>
2025-02-17 23:28:51 -0300

    💄 Views: Main

--- vet-care/resources/views/layout/main.blade.php
+++ vet-care/resources/views/layout/main.blade.php
@@ -48,6 +48,9 @@
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
    <script src="//cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
    <script src="https://cdn.datatables.net/1.13.8/js/dataTables.bootstrap5.min.js"></script>
+
+   @component('layout.footer')
+   @endcomponent
 </body>
 
 </html>
